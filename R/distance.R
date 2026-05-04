6253f75281b4c6e362fbe621272ab684e5b5527f
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2026-05-04 09:37:13 +0100

    Adjusted indentation for better clarity and consistency in the impute_query_exp_value() function using {styler}

--- R/distance.R
+++ R/distance.R
@@ -169,18 +169,18 @@ impute_query_exp_value <- function(data_query) {
   imputed_list <- lapply(
     genes,
     function(gene) {
-        data <- data_query[data_query$gene_query == gene]
+      data <- data_query[data_query$gene_query == gene]
 
-        timepoint_range <- data[, .(min_t = ceiling(min(timepoint_reg)), max_t = floor(max(timepoint_reg)))]
-        timepoint_range_seq <- timepoint_range$min_t:timepoint_range$max_t
+      timepoint_range <- data[, .(min_t = ceiling(min(timepoint_reg)), max_t = floor(max(timepoint_reg)))]
+      timepoint_range_seq <- timepoint_range$min_t:timepoint_range$max_t
 
-        interp_data <- data.table::data.table(
-          gene_query = gene,
-          timepoint_query = timepoint_range_seq,
-          exp_query = stats::approxfun(data$timepoint_reg, data$exp_query)(timepoint_range_seq)
-        )
+      interp_data <- data.table::data.table(
+        gene_query = gene,
+        timepoint_query = timepoint_range_seq,
+        exp_query = stats::approxfun(data$timepoint_reg, data$exp_query)(timepoint_range_seq)
+      )
 
-        return(interp_data)
+      return(interp_data)
     }
   )
 
